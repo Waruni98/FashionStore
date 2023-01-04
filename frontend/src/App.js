6457095f4aa34c11cc5798720825d57fdd0c30a0
Waruni98 <it20666538@my.sliit.lk>
2023-01-05 02:10:18 +0530

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div >
+      <header >
+       <a href="/">Fashion Store</a>
+      </header>
+      <main>List Product</main>
+    </div>
+  );
+}
+
+export default App;
