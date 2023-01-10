35ddf4e8d08c9b0dd41c2a930e1c9f7494f262af
Waruni98 <it20666538@my.sliit.lk>
2023-01-10 12:26:59 +0530

    create front page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
-
+import data from './data';
 
 function App() {
   return (
-    <div >
-      <header >
-       <a href="/">Fashion Store</a>
+    <div>
+      <header>
+        <a href="/">Fashion Store</a>
       </header>
-      <main>List Product</main>
+      <main>
+        <h1>Featured products</h1>
+        <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <a href={'/product/${product.slug}'}>
+            <img src={product.image} alt={product.name} />
+            </a>
+            <div className="product-infor">
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+            <button>Add to Cart</button>
+            </div>
+          </div>
+        ))}
+        </div>
+      </main>
     </div>
   );
 }
